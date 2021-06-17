8c58c8fb5a3b9593b3f9c51530102043745a4ad2
송가영 <sgyoung96@naver.com>
2021-06-17 18:23:00 +0900

    메인 액티비티 상단 툴바 텍스트 바꾸기 실패

--- ver1/app/src/main/java/com/example/diary1/ui/activity/MainPageActivity.kt
+++ ver1/app/src/main/java/com/example/diary1/ui/activity/MainPageActivity.kt
@@ -2,6 +2,7 @@ package com.example.diary1.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import com.example.diary1.R
 import com.example.diary1.ui.fragment.CalendarFragment
@@ -16,27 +17,33 @@ class MainPageActivity : AppCompatActivity(), BottomNavigationView.OnNavigationI
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_page)
 
+        // 왜 안바껴....
+        try {
+            tb_title.title = "${R.string.title_daily_diary_list}"
+        } catch (e: Exception) {
+            Log.d("TITLE SETTING FAIL", ">>>>>>>>>>$e")
+        }
+
         bn_bottom_icons.setOnNavigationItemSelectedListener(this)
 
-        tb_title.setTitle(R.string.title_daily_diary_list)
         supportFragmentManager.beginTransaction().add(R.id.vg_fragment_container, DiaryListFragment()).commit()
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.bottom_item_list -> {
-                supportFragmentManager.beginTransaction().replace(R.id.vg_fragment_container, DiaryListFragment()).commitAllowingStateLoss()
                 tb_title.setTitle(R.string.title_daily_diary_list)
+                supportFragmentManager.beginTransaction().replace(R.id.vg_fragment_container, DiaryListFragment()).commitAllowingStateLoss()
                 return true
             }
             R.id.bottom_item_post -> {
-                supportFragmentManager.beginTransaction().replace(R.id.vg_fragment_container, PostDiaryFragment()).commitAllowingStateLoss()
                 tb_title.setTitle(R.string.title_post_diary)
+                supportFragmentManager.beginTransaction().replace(R.id.vg_fragment_container, PostDiaryFragment()).commitAllowingStateLoss()
                 return true
             }
             R.id.bottom_item_calendar -> {
-                supportFragmentManager.beginTransaction().replace(R.id.vg_fragment_container, CalendarFragment()).commitAllowingStateLoss()
                 tb_title.setTitle(R.string.title_calendar)
+                supportFragmentManager.beginTransaction().replace(R.id.vg_fragment_container, CalendarFragment()).commitAllowingStateLoss()
                 return true
             }
         }
