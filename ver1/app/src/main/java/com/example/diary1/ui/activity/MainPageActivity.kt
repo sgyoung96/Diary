0cb04e10239bc5a2eb6bc2baade92d1aeddd1353
송가영 <sgyoung96@naver.com>
2021-06-21 17:44:37 +0900

    BottomNavigationView icon, text 선택시 색상 바뀌도록 하는 것 실패.. 타이틀 바꾸기 실패

--- ver1/app/src/main/java/com/example/diary1/ui/activity/MainPageActivity.kt
+++ ver1/app/src/main/java/com/example/diary1/ui/activity/MainPageActivity.kt
@@ -1,5 +1,6 @@
 package com.example.diary1.ui.activity
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -12,6 +13,11 @@ import com.example.diary1.ui.fragment.TestFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main_page.*
 
+/**
+ * 해야할 일
+ * Toolbar kotlin 예제 책 보고 한 대로 만들어서 타이틀 바꾸기
+ * BottomNavigationView 아이콘과 텍스트 선택하면 색상 바뀌도록
+ */
 class MainPageActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,12 +32,23 @@ class MainPageActivity : AppCompatActivity(), BottomNavigationView.OnNavigationI
 
         bn_bottom_icons.setOnNavigationItemSelectedListener(this)
 
+        bn_bottom_icons.itemIconTintList = getColorStateList(R.color.main_highlight)
+        bn_bottom_icons.itemTextColor = getColorStateList(R.color.main_highlight)
+
+
         supportFragmentManager.beginTransaction().add(R.id.vg_fragment_container, DiaryListFragment()).commit()
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.bottom_item_list -> {
+                if (bn_bottom_icons.isSelected) {
+                    bn_bottom_icons.itemIconTintList = getColorStateList(R.color.input_highlight)
+                    bn_bottom_icons.itemTextColor = getColorStateList(R.color.input_highlight)
+                } else {
+                    bn_bottom_icons.itemIconTintList = getColorStateList(R.color.main_highlight)
+                    bn_bottom_icons.itemTextColor = getColorStateList(R.color.main_highlight)
+                }
                 tb_title.setTitle(R.string.title_daily_diary_list)
                 supportFragmentManager.beginTransaction().replace(R.id.vg_fragment_container, DiaryListFragment()).commitAllowingStateLoss()
                 return true
