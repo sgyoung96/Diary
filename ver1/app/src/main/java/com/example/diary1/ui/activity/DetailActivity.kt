564165d437bae188a874ee5a26cf79fa63356f6e
송가영 <sgyoung96@naver.com>
2021-06-28 16:06:14 +0900

    DiaryList item 디자인 완료

---
+++ ver1/app/src/main/java/com/example/diary1/ui/activity/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.diary1.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.diary1.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
