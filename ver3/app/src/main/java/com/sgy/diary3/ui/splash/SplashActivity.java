8d721cd9c4638b0ab5334145244f0ad7c6f1ac2f
송가영 <sgyoung96@naver.com>
2021-11-28 23:21:27 +0900

    TODO 추가

--- ver3/app/src/main/java/com/sgy/diary3/ui/splash/SplashActivity.java
+++ ver3/app/src/main/java/com/sgy/diary3/ui/splash/SplashActivity.java
@@ -3,7 +3,7 @@ package com.sgy.diary3.ui.splash;
 import com.sgy.diary3.base.BaseActivity;
 import com.sgy.diary3.base.MyApplication;
 import com.sgy.diary3.util.Utils;
-
+// TODO Deprecated 된 Intent 찾기, 웹포토샵작업(배경 밝게) + Lottie Animation 추가하기 + Navigation bar 는 회색으로 나타나도록 수정
 public class SplashActivity extends BaseActivity {
     @Override
     public void setStatusbarPadding(int height) {
